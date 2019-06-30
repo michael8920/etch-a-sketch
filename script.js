108e70e698b6a9cf17465ae2445b12087ae89652
Michael <michal.sobocinski@zoho.com>
2019-06-30 19:54:22 +0100

    Hover effect added

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 const container = document.querySelector('#container');
+let gridItems = [];
 
-// ADDING SINGLE DIV TO MY GRID CONTAINER
-//const gridItem = document.createElement('div');
-//gridItem.style.cssText = 'height: 50px; width: 50px; grid-area: 1/1/2/2; border: 1px solid blue;';
-//container.appendChild(gridItem);
-
-//TRYING TO FIGURE OUT FUNCTION TO FILL ALL GRID SLOTS 
-
-function fillGrid () { 
-    let gridItems = [];
-    for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 256; i++) {
         gridItems[i] = document.createElement('div');
         gridItems[i].style.cssText = 'height: 50px; width: 50px; border: 1px solid blue; grid-auto-flow: row;';
         container.appendChild(gridItems[i]);
-    }
 }
 
-fillGrid();
+let itemsArray = Array.from(gridItems);
+
+itemsArray.forEach(function(element) {
+    element.addEventListener('mouseover', function() {
+        element.style.cssText = 'background-color: black;'
+    })
+})
