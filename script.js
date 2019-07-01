d40bd913a1c4c478fdeed6a920274be5f6ee0f74
Michael <michal.sobocinski@zoho.com>
2019-07-01 21:48:39 +0100

    Completed JS

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('#gridreset');
 let gridItems = [];
+let cleanGrid = [];
+
 
 for (let i = 0; i < 256; i++) {
         gridItems[i] = document.createElement('div');
-        gridItems[i].style.cssText = 'height: 50px; width: 50px; border: 1px solid blue; grid-auto-flow: row;';
+        gridItems[i].style.cssText = 'border: 1px solid blue; grid-auto-flow: row;';
         container.appendChild(gridItems[i]);
 }
 
@@ -14,3 +17,23 @@ itemsArray.forEach(function(element) {
         element.style.cssText = 'background-color: black;'
     })
 })
+
+function newGrid () {
+    let userGrid = prompt('How many squares per side do you want your new grid to be?');
+    container.innerHTML = "";
+
+    for (let i = 0; i < (userGrid * userGrid); i++) {
+        cleanGrid[i] = document.createElement('div');
+        cleanGrid[i].style.cssText = 'border: 1px solid blue; grid-auto-flow: row;';
+        container.appendChild(cleanGrid[i]);    
+    }
+    container.setAttribute('style', `grid-template-columns: repeat(${userGrid}, auto); grid-template-rows: repeat(${userGrid}, auto);`);
+
+    cleanGrid.forEach(function(element) {
+    element.addEventListener('mouseover', function() {
+        element.style.cssText = 'background-color: black;'
+    })
+    })
+}
+
+button.addEventListener('click', newGrid);
