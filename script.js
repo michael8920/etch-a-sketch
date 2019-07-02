74eb571378bae26746387499422841412bed2dca
Michael <michal.sobocinski@zoho.com>
2019-07-02 13:00:09 +0100

    Completed Project

--- script.js
+++ script.js
@@ -1,39 +1,100 @@
 const container = document.querySelector('#container');
 const button = document.querySelector('#gridreset');
+const pencilBtn = document.querySelector('#pencil');
+
 let gridItems = [];
-let cleanGrid = [];
 
+button.addEventListener('click', newGrid);
+pencilBtn.addEventListener('click', pencilChoice);
+pencilBtn.addEventListener('click', changeText);
 
-for (let i = 0; i < 256; i++) {
+// Initial function written to give users an overview on how the sketch book looks and how it works 
+
+function initialGrid (userGrid) {
+    for (let i = 0; i < (userGrid * userGrid); i++) {
         gridItems[i] = document.createElement('div');
-        gridItems[i].style.cssText = 'border: 1px solid blue; grid-auto-flow: row;';
-        container.appendChild(gridItems[i]);
+        gridItems[i].style.cssText = 'box-sizing: border-box; border: 0.5px solid rgba(255,255,255,0.15); grid-auto-flow: row';
+        container.appendChild(gridItems[i]);    
+    }
+    container.setAttribute('style', `grid-template-columns: repeat(${userGrid}, auto); grid-template-rows: repeat(${userGrid}, auto);`);
+    
+    gridItems.forEach(function(element) {
+        element.addEventListener('mouseover', function() {
+            element.style.cssText = 'background-color: black;'
+        })
+    })
 }
 
-let itemsArray = Array.from(gridItems);
+initialGrid(16);
+// End of initial function 
 
-itemsArray.forEach(function(element) {
-    element.addEventListener('mouseover', function() {
-        element.style.cssText = 'background-color: black;'
-    })
-})
+
+// Function written to allow users create their own grids 
 
 function newGrid () {
-    let userGrid = prompt('How many squares per side do you want your new grid to be?');
-    container.innerHTML = "";
+    container.innerHTML = ""; // Clear our container from the initial gridItems 
+    gridItems.length = 0; // Clear our array from initial elements
+    let userGrid = prompt('How many squares per side do you want your new grid to be');
 
-    for (let i = 0; i < (userGrid * userGrid); i++) {
-        cleanGrid[i] = document.createElement('div');
-        cleanGrid[i].style.cssText = 'border: 1px solid blue; grid-auto-flow: row;';
-        container.appendChild(cleanGrid[i]);    
+    for(let i = 0; i < userGrid * userGrid; i++) {
+        gridItems[i] = document.createElement('div');
+        gridItems[i].style.cssText = 'box-sizing: border-box; border: 0.5px solid rgba(255,255,255,0.15); grid-auto-flow: row;'
+        container.appendChild(gridItems[i]);
     }
     container.setAttribute('style', `grid-template-columns: repeat(${userGrid}, auto); grid-template-rows: repeat(${userGrid}, auto);`);
 
-    cleanGrid.forEach(function(element) {
-    element.addEventListener('mouseover', function() {
-        element.style.cssText = 'background-color: black;'
-    })
-    })
+    if (pencilBtn.textContent === 'BLACK') {
+        gridItems.forEach(function(element) {
+          element.addEventListener('mouseover', function() {
+            randomColor();
+            element.style.cssText = `background-color: ${bgColor};`;
+          })
+        })
+    } else {
+        gridItems.forEach(function(element) {
+            element.addEventListener('mouseover', function() {
+                element.style.cssText = 'background-color: black;'
+            })
+        })
+    }
 }
 
-button.addEventListener('click', newGrid);
+// Function written to change text on the pencil button when clicked
+
+function changeText () {
+    if (pencilBtn.textContent === 'RANDOM COLOR') {
+        pencilBtn.textContent = 'BLACK'
+    } else {
+        pencilBtn.textContent = 'RANDOM COLOR'
+    }
+}
+// Function written to allow users change pencils between black and random color
+
+function pencilChoice () {
+  if (pencilBtn.textContent === 'RANDOM COLOR') {
+      gridItems.forEach(function(element) {
+        element.addEventListener('mouseover', function() {
+          randomColor();
+          element.style.cssText = `background-color: ${bgColor};`
+        })
+      })
+  } else if (pencilBtn.textContent === 'BLACK') {
+      gridItems.forEach(function(element) {
+        element.addEventListener('mouseover', function() {
+          element.style.cssText = 'background-color: black;'    
+        })
+      })
+  }
+} 
+
+// Function written to get random rgb color 
+let bgColor = '';
+
+function randomColor () {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    
+}
+
